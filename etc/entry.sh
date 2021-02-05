9adc7873a000f27e45e05d00cf346f1bee787c9b
Bradley Griffin <bradley@griffindork.com>
2021-02-05 12:53:01 -0800

    Fix entry

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,7 +3,7 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit
 
-${STEAMAPPDIR}/valheim_server.x86_64 -name "${NAME}" -port 2456 -world "${WORLD}" -password "${PASSWORD}" -public 1 > /dev/null &
+bash "${STEAMAPPDIR}/valheim_server.x86_64" -name "${NAME}" -port 2456 -world "${WORLD}" -password "${PASSWORD}" -public 1 > /dev/null &
 
 export LD_LIBRARY_PATH=$templdpath
 
