ddd56e4cd05c6e6b500176ea3fc6d3852b2f4361
Bradley Griffin <bradley@griffindork.com>
2021-02-05 16:53:18 -0800

    Change launching

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,6 +3,6 @@ export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 bash "${STEAMCMDDIR}/steamcmd.sh" +login anonymous +force_install_dir "${STEAMAPPDIR}" +app_update "${STEAMAPPID}" +quit
 
-bash "${STEAMAPPDIR}/valheim_server.x86_64" -name "${NAME}" -port "${PORT}" -world "${WORLD}" -password "${PASSWORD}" -public 1
+"${STEAMAPPDIR}/valheim_server.x86_64" -name "${NAME}" -port "${PORT}" -world "${WORLD}" -password "${PASSWORD}" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
